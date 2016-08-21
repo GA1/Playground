e3bdf2d5331fac01baf233f0603bc4b8e887ec97
Kamil Nguyen Van <knguyen@zinio.com>
2016-08-21 22:05:44 +0200

    Run on 8001

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
   res.json({"message":"Hello World!"})
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(8001, function () {
+  console.log('Example app listening on port 8001!');
 });
